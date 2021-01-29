50b6b73fde51a23d024d6511fab103f4166fae82
AanMFA <aanmfa06@gmail.com>
2021-01-29 10:04:05 +0700

    Add Creating Folder Backend(#8)

--- app/src/main/java/com/dokumenku/meeting/modul/dashboard/HomeActivity.kt
+++ app/src/main/java/com/dokumenku/meeting/modul/dashboard/HomeActivity.kt
@@ -1,15 +1,42 @@
 package com.dokumenku.meeting.modul.dashboard
 
+import android.util.Log
+import com.bumptech.glide.Glide
+import com.dokumenku.meeting.R
 import com.dokumenku.meeting.base.BaseFragmentHolderActivity
 import com.dokumenku.meeting.modul.dashboard.pdflist.PdfListFragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 
-class HomeActivity : BaseFragmentHolderActivity(){
+
+class HomeActivity : BaseFragmentHolderActivity(), HomeContract.View{
+    private val presenter = HomePresenter(this)
+
     companion object{
         private const val TAG = "HomeActivity"
+    }
+
+    override fun initializeView() {
+        super.initializeView()
+        loadGoogleAccountPhoto()
     }
 
     override fun initializeFragment() {
         initializeView()
         setCurrentFragment(PdfListFragment(), true)
+    }
+
+    override fun redirectToProfilePage() {
+        // TODO("Not yet implemented")
+    }
+
+    override fun loadGoogleAccountPhoto() {
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+
+        if (acct != null) {
+            Log.d(TAG, "Loading photo from url : ${acct.photoUrl.toString()}")
+            Glide.with(this)
+                .load(acct.photoUrl)
+                .into(findViewById(R.id.profile_photo))
+        }
     }
 }
---
+++ app/src/main/java/com/dokumenku/meeting/modul/dashboard/HomeContract.kt
@@ -0,0 +1,14 @@
+package com.dokumenku.meeting.modul.dashboard
+
+import com.dokumenku.meeting.base.BasePresenter
+
+interface HomeContract {
+    interface Presenter : BasePresenter {
+        fun syncData()
+    }
+
+    interface View{
+        fun redirectToProfilePage()
+        fun loadGoogleAccountPhoto()
+    }
+}
